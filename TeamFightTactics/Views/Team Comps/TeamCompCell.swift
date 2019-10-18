c2183da62401e4686ae3b2b508996fd7f8b7153b
Alex Cooper <Cooparr@hotmail.co.uk>
2019-10-18 19:12:40 +0100

    Began to flesh out the custom tableviewcells. Implemented the tier labels and began working on the little team composition images.

--- TeamFightTactics/Views/Team Comps/TeamCompCell.swift
+++ TeamFightTactics/Views/Team Comps/TeamCompCell.swift
@@ -7,38 +7,137 @@
 //
 
 import UIKit
+import SDWebImage
 
 class TeamCompCell: UITableViewCell {
+    
+    let placeholderImage = UIImage(named: "Neeko.png")
     
     var teamComp: TeamComposition? {
         didSet {
             guard
-                let title = teamComp?.title
+                let title = teamComp?.title,
+                let tier = teamComp?.tier,
+                let champions = teamComp?.endGame
                 else { return }
             
-            testLabel.text = title
+            titleLabel.text = title
+            
+            
+            
+            champions.forEach { (champ) in
+                if let champName = champ["name"] {
+                    champImageOne.sd_setImage(with: URL(string: "https://ddragon.leagueoflegends.com/cdn/9.13.1/img/champion/\(champName).png"), placeholderImage: placeholderImage)
+                }
+            }
+            
+            setTierLabelAndColor(tier)
         }
     }
+    
     
     //MARK:- Override Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-
-        self.addSubview(testLabel)
-        testLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        testLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-        testLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        testLabel.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        setupCell()
+        setupCellContent()
+    }
+    
+    //MARK: Set Tier Label And Color
+    fileprivate func setTierLabelAndColor(_ tier: Int) {
+        switch tier {
+        case 0:
+            teamCompTier.text = "S Tier"
+            teamCompTier.backgroundColor = CustomColor.sTier
+        case 1:
+            teamCompTier.text = "A Tier"
+            teamCompTier.backgroundColor = CustomColor.aTier
+        case 2:
+            teamCompTier.text = "B Tier"
+            teamCompTier.backgroundColor = CustomColor.bTier
+        case 3:
+            teamCompTier.text = "C Tier"
+            teamCompTier.backgroundColor = CustomColor.cTier
+        case 4:
+            teamCompTier.text = "D Tier"
+            teamCompTier.backgroundColor = CustomColor.dTier
+        default:
+            teamCompTier.text = "E Tier"
+            teamCompTier.backgroundColor = CustomColor.eTier
+        }
     }
     
     
-    let testLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.preferredFont(forTextStyle: .title2)
-        return label
+    //MARK:- Team Comp Title & Tier
+    let titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        return lbl
     }()
+    
+    let teamCompTier: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textColor = CustomColor.richBlack
+        lbl.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        lbl.textAlignment = .center
+        lbl.clipsToBounds = true
+        lbl.layer.cornerRadius = 6.0
+        lbl.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMinYCorner]
+        return lbl
+    }()
+    
+    //MARK:- Champ Images
+    let champImageOne: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.contentMode = .scaleAspectFit
+        imgView.layer.borderWidth = 1.0
+        imgView.layer.cornerRadius = 2.0
+        
+        imgView.layer.borderColor = UIColor.red.cgColor
+        
+        return imgView
+    }()
+    
+    
+    //MARK:- Setup Cell
+    fileprivate func setupCell() {
+        accessoryType = .disclosureIndicator
+        backgroundColor = CustomColor.richBlack
+        separatorInset.left = 0
+        
+    }
+    
+    
+    //MARK:- Setup Cell Content
+    fileprivate func setupCellContent() {
+        
+        
+        //MARK: Title & Tier
+        addSubview(titleLabel)
+        addSubview(teamCompTier)
+        
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        
+        teamCompTier.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        teamCompTier.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        teamCompTier.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        teamCompTier.heightAnchor.constraint(equalToConstant: 17).isActive = true
+        
+        
+        //MARK: Champ Images
+        addSubview(champImageOne)
+        
+        let champImgSize: CGFloat = 30
+        champImageOne.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        champImageOne.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        champImageOne.heightAnchor.constraint(equalToConstant: champImgSize).isActive = true
+        champImageOne.widthAnchor.constraint(equalToConstant: champImgSize).isActive = true
+
+    }
     
     
     // Required
