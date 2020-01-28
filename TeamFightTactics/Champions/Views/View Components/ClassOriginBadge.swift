4945698c9bebac86dead65da48c591974117f243
Alex Cooper <Cooparr@hotmail.co.uk>
2020-01-28 20:03:51 +0000

    Modified the synergy badge code to better display the new png class origin badges, also added some new item images that I will be using shortly.

--- TeamFightTactics/Champions/Views/View Components/ClassOriginBadge.swift
+++ TeamFightTactics/Champions/Views/View Components/ClassOriginBadge.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ClassOriginBadge: UIView {
 
+    //MARK:- Properties
     let typeLabel: UILabel = {
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
@@ -26,28 +27,37 @@ class ClassOriginBadge: UIView {
         return imgView
     }()
     
+    
+    //MARK:- Override Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = CustomColor.romanSilver
         layer.cornerRadius = 2.0
+        heightAnchor.constraint(equalToConstant: 17).isActive = true
         
+        setupIconAndLabel()
+    }
+    
+    
+    //MARK:- Setup Icon & Label
+    fileprivate func setupIconAndLabel() {
         addSubview(typeIcon)
         addSubview(typeLabel)
         
         NSLayoutConstraint.activate([
-          typeIcon.heightAnchor.constraint(equalToConstant: 17),
-            typeIcon.widthAnchor.constraint(equalToConstant: 15),
+            typeIcon.heightAnchor.constraint(equalToConstant: 12.5),
+            typeIcon.widthAnchor.constraint(equalToConstant: 12.5),
             typeIcon.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2),
-            typeIcon.topAnchor.constraint(equalTo: topAnchor),
-            typeIcon.trailingAnchor.constraint(equalTo: typeLabel.leadingAnchor),
-            typeIcon.bottomAnchor.constraint(equalTo: bottomAnchor),
-            typeLabel.leadingAnchor.constraint(equalTo: typeIcon.trailingAnchor),
+            typeIcon.centerYAnchor.constraint(equalTo: centerYAnchor),
+            typeIcon.trailingAnchor.constraint(equalTo: typeLabel.leadingAnchor, constant: -2),
+            typeLabel.leadingAnchor.constraint(equalTo: typeIcon.trailingAnchor, constant: 2),
             typeLabel.topAnchor.constraint(equalTo: topAnchor),
             typeLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
             typeLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
