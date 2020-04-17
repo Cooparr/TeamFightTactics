19a62d3ee16fd1f9560bbaaa55616feaba355d10
Alex Cooper <Cooparr@hotmail.co.uk>
2020-04-17 15:06:57 +0100

    Adjusted convenience init to take in width and height, not sure this is the best solution but currently works. Removed the isHidden property

--- TeamFightTactics/Team Compositions/View/View Components/TCDetailChampImage.swift
+++ TeamFightTactics/Team Compositions/View/View Components/TCDetailChampImage.swift
@@ -14,23 +14,22 @@ class TCDetailChampImage: UIImageView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        contentMode = .scaleAspectFit
         clipsToBounds = true
+        contentMode = .scaleAspectFit
         layer.borderWidth = 1.0
         layer.cornerRadius = 2.0
         layer.borderColor = CustomColor.romanSilver.cgColor
-        isHidden = true
-        
+    }    
+    
+    //MARK:- Convinence Init    
+    convenience init(width: CGFloat, height: CGFloat) {
+      self.init(frame: CGRect.zero)
         NSLayoutConstraint.activate([
-            heightAnchor.constraint(equalToConstant: 35),
-            widthAnchor.constraint(equalToConstant: 35)
+            widthAnchor.constraint(equalToConstant: width),
+            heightAnchor.constraint(equalToConstant: height)
         ])
     }
     
-    //MARK:- Convinence Init
-    convenience init() {
-        self.init(frame: CGRect.zero)
-    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
