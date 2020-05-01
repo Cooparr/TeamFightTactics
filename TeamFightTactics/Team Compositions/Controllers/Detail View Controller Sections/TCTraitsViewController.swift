6034bd8f0405add7ecef7a15beca1d6bda04df26
Alex Cooper <Cooparr@hotmail.co.uk>
2020-05-01 17:21:33 +0100

    TraitsViewController receives the classes and origins it needs through property dependency injection. They are then passed into he createTraitTilesForStackView function to create a trait tile on the TCDetail View page for a given team comp. This is added to a stack view to handle the dynamic layout.

---
+++ TeamFightTactics/Team Compositions/Controllers/Detail View Controller Sections/TCTraitsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TCTraitsViewController.swift
+//  TeamFightTactics
+//
+//  Created by Alexander James Cooper on 28/04/2020.
+//  Copyright © 2020 Alexander James Cooper. All rights reserved.
+//
+
+import UIKit
+
+class TCTraitsViewController: UIViewController {
+
+    //MARK: Properties
+    var classObjs = [Trait]()
+    var originObjs = [Trait]()
+    var synergies = [TeamCompositionSynergies]()
+    lazy private var traitsSectionView: TCTraitsView = TCTraitsView()
+    
+    
+    //MARK:- Load View
+    override func loadView() {
+        super.loadView()
+        self.view = self.traitsSectionView
+    }
+    
+    
+    //MARK:- View Did Load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createTraitTilesForStackView()
+    }
+    
+    
+    //MARK:- Creat Trait Tile
+    fileprivate func createTraitTilesForStackView() {
+        for trait in classObjs + originObjs {
+            for syn in synergies where syn.name.contains(trait.name) {
+                let traitInfo = TCDetailTraitInfo()
+                setTraitSynergyBadge(traitInfo, trait, syn)
+                setTraitEffectLabel(trait, traitInfo)
+                setTileBonusLabels(trait, syn, traitInfo)
+                traitsSectionView.synergiesStackView.addArrangedSubview(traitInfo)
+            }
+        }
+    }
+    
+    
+    //MARK: Set Tile Synergy Badge
+    fileprivate func setTraitSynergyBadge(_ traitInfo: TCDetailTraitInfo, _ trait: Trait, _ syn: TeamCompositionSynergies) {
+        traitInfo.synergyBadge.synergyIcon.image = UIImage(named: "\(trait.name)")
+        traitInfo.synergyBadge.synergyNameLabel.text = trait.name
+        traitInfo.synergyBadge.backgroundColor = syn.rank.setBadgeColor()
+    }
+    
+    
+    //MARK: Set Tile Effect Label
+    fileprivate func setTraitEffectLabel(_ trait: Trait, _ traitInfo: TCDetailTraitInfo) {
+        if trait.effect == nil {
+            traitInfo.synergyEffect.isHidden = true
+        }
+        traitInfo.synergyEffect.text = trait.effect
+    }
+    
+    
+    //MARK: Set Tile Bonus Labels
+    fileprivate func setTileBonusLabels(_ trait: Trait, _ syn: TeamCompositionSynergies, _ traitInfo: TCDetailTraitInfo) {
+        if let bonus = trait.bonus.reversed().first(where: { $0.count <= syn.count }) {
+            traitInfo.synergyCount.text = "\(bonus.count)"
+            traitInfo.synergyCount.layer.borderColor = syn.rank.setBadgeColor().cgColor
+            traitInfo.synergyValue.text = "\(bonus.value)"
+        }
+    }
+}
