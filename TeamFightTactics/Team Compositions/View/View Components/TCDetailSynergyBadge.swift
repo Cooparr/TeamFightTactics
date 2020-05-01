348cd7ecca4ff820116a0c86e9479a7c45b95c7c
Alex Cooper <Cooparr@hotmail.co.uk>
2020-05-01 17:04:19 +0100

    Created the trait info View that will be used to display class and origin information within the TC detail view. Also created a different synergy badge specific for this section.

---
+++ TeamFightTactics/Team Compositions/View/View Components/TCDetailSynergyBadge.swift
@@ -0,0 +1,77 @@
+//
+//  TCDetailSynergyBadge.swift
+//  TeamFightTactics
+//
+//  Created by Alexander James Cooper on 29/04/2020.
+//  Copyright © 2020 Alexander James Cooper. All rights reserved.
+//
+
+import UIKit
+
+class TCDetailSynergyBadge: UIView {
+    
+    //MARK:- Properties
+    let synergyIcon: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.contentMode = .scaleAspectFit
+        imgView.tintColor = CustomColor.platinum
+        return imgView
+    }()
+    
+    let synergyNameLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.textColor = CustomColor.platinum
+        lbl.textAlignment = .center
+        lbl.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        return lbl
+    }()
+    
+    
+    //MARK:- Override Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupBadge()
+        setupIconAndLabelSubviews()
+        
+        
+        backgroundColor = CustomColor.goldSynergy
+    }
+    
+    
+    //MARK:- Setup Synergry Badge View
+    fileprivate func setupBadge() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 2.0
+    }
+    
+    
+    //MARK:- Setup Subviews & Constraints
+    fileprivate func setupIconAndLabelSubviews() {
+        let iconSize: CGFloat = 20
+        let padding: CGFloat = 5
+        
+        addSubview(synergyIcon)
+        NSLayoutConstraint.activate([
+            synergyIcon.widthAnchor.constraint(equalToConstant: iconSize),
+            synergyIcon.heightAnchor.constraint(equalToConstant: iconSize),
+            synergyIcon.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            synergyIcon.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            synergyIcon.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding)
+        ])
+        
+        addSubview(synergyNameLabel)
+        NSLayoutConstraint.activate([
+            synergyNameLabel.leadingAnchor.constraint(equalTo: synergyIcon.trailingAnchor, constant: padding),
+            synergyNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            synergyNameLabel.topAnchor.constraint(equalTo: topAnchor),
+            synergyNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ TeamFightTactics/Team Compositions/View/View Components/TCDetailTraitInfo.swift
@@ -0,0 +1,123 @@
+//
+//  TCDetailTraitInfo.swift
+//  TeamFightTactics
+//
+//  Created by Alexander James Cooper on 30/04/2020.
+//  Copyright © 2020 Alexander James Cooper. All rights reserved.
+//
+
+import UIKit
+
+class TCDetailTraitInfo: UIView {
+    
+    //MARK: StackViews
+    let traitVertStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .leading
+        stack.axis = .vertical
+        stack.spacing = 8
+        return stack
+    }()
+    
+    let traitBonusStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.alignment = .leading
+        stack.axis = .horizontal
+        stack.spacing = 8
+        return stack
+    }()
+    
+    //MARK: Label & ImageViews
+    let synergyBadge = TCDetailSynergyBadge()
+    let synergyEffect: UILabel = {
+        let lbl = UILabel()
+        lbl.font = .systemFont(ofSize: 14, weight: .regular)
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        return lbl
+    }()
+    
+    let synergyCount: UILabel = {
+        let size: CGFloat = 20
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            lbl.heightAnchor.constraint(equalToConstant: size),
+            lbl.widthAnchor.constraint(equalToConstant: size)
+        ])
+        
+        lbl.font = .systemFont(ofSize: 14, weight: .regular)
+        lbl.textAlignment = .center
+        lbl.backgroundColor = CustomColor.richBlack
+        lbl.layer.cornerRadius = size / 2
+        lbl.layer.masksToBounds = true
+        lbl.layer.borderWidth = 1
+        return lbl
+    }()
+    
+    let synergyValue: UILabel = {
+        let lbl = UILabel()
+        lbl.font = .systemFont(ofSize: 14, weight: .regular)
+        lbl.numberOfLines = 0
+        lbl.lineBreakMode = .byWordWrapping
+        return lbl
+    }()
+    
+    
+    //MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupTraitInfo()
+        
+        let padding: CGFloat = 8
+        setupSynergyBadge(padding)
+        setupTraitStackViews(padding)
+    }
+    
+    
+    //MARK: Setup Trait Info
+    fileprivate func setupTraitInfo() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = CustomColor.charcoal
+        layer.cornerRadius = 5
+    }
+
+    
+    //MARK: Setup Synergy Badge
+    fileprivate func setupSynergyBadge(_ padding: CGFloat) {
+        addSubview(synergyBadge)
+        NSLayoutConstraint.activate([
+            synergyBadge.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            synergyBadge.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8)
+        ])
+    }
+    
+    
+    //MARK: Setup Trait Stack Views
+    fileprivate func setupTraitStackViews(_ padding: CGFloat) {
+        addSubview(traitVertStack)
+        traitVertStack.addArrangedSubview(synergyEffect)
+        traitVertStack.addArrangedSubview(traitBonusStack)
+        NSLayoutConstraint.activate([
+            traitVertStack.topAnchor.constraint(equalTo: synergyBadge.bottomAnchor, constant: 8),
+            traitVertStack.leadingAnchor.constraint(equalTo: synergyBadge.leadingAnchor, constant: 8),
+            traitVertStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+            traitVertStack.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
+        ])
+        
+        traitBonusStack.addArrangedSubview(synergyCount)
+        traitBonusStack.addArrangedSubview(synergyValue)
+        NSLayoutConstraint.activate([
+            traitBonusStack.leadingAnchor.constraint(equalTo: traitVertStack.leadingAnchor),
+            traitBonusStack.trailingAnchor.constraint(equalTo: traitVertStack.trailingAnchor)
+        ])
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
