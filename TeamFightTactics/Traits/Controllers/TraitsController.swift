6c5b404c16c4306770d803e2c6e19c35950fbbf9
Alex Cooper <Cooparr@hotmail.co.uk>
2020-05-13 12:57:02 +0100

    Renamed a few tab bar image assets, and created an enum storing these assets for use within the app. Also created a TraitsController, which will be used next to display classes and origins.

---
+++ TeamFightTactics/Traits/Controllers/TraitsController.swift
@@ -0,0 +1,13 @@
+//
+//  TraitsController.swift
+//  TeamFightTactics
+//
+//  Created by Alexander James Cooper on 11/05/2020.
+//  Copyright © 2020 Alexander James Cooper. All rights reserved.
+//
+
+import UIKit
+
+class TraitsController: UIViewController {
+    
+}
