603a56748f9f0d27ff6e50b5b7c3358a2905ebd9
Alex Cooper <Cooparr@hotmail.co.uk>
2020-06-05 13:35:22 +0100

    ItemsStats now parses correctly, still need to make some changes on the backend so this model could still change in future.

--- TeamFightTactics/Items/Models/Item.swift
+++ TeamFightTactics/Items/Models/Item.swift
@@ -8,15 +8,14 @@
 
 import Foundation
 
-//FIXME: stats currently does not parse
 struct Item: DictionaryInitialize {
     
     let id, tier: Int
     let name, description: String
     let into: [String]?
     let from: [Int]?
-    let stats: [ItemStat]
-    
+    var stats: [ItemStat] = []
+
     init(data: [String: Any]) {
         self.id = data["id"] as? Int ?? -1
         self.name = data["name"] as? String ?? ""
@@ -24,27 +23,23 @@ struct Item: DictionaryInitialize {
         self.tier = data["tier"] as? Int ?? -1
         self.into = data["into"] as? [String]
         self.from = data["from"] as? [Int]
-        self.stats = data["stats"] as? [ItemStat] ?? [ItemStat(data: ["" : ""])]
+        let stats = data["stats"] as? [[String: String]] ?? [["": ""]]
         
-        
-        print("\nid:", self.id)
-        print("name:", self.name)
-        print("desc:", self.description)
-        print("tier:", self.tier)
-        print("into:", self.into as Any)
-        print("from:", self.from as Any)
-        print("stats:", self.stats)
+        stats.forEach { stat in
+            let itemStat = ItemStat(data: stat)
+            self.stats.append(itemStat)
+        }
     }
 }
 
 
 struct ItemStat {
     
-    let name, value: String
-    
+    let name: String
+    let value: String
+
     init(data: [String: String]) {
         self.name = data["name"] ?? ""
         self.value = data["value"] ?? ""
     }
-    
 }
