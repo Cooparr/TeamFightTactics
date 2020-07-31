c68e5dcabf21b4a5635ac13cf99276a11b6381b2
Alex Cooper <Cooparr@hotmail.co.uk>
2020-07-31 06:14:59 +0100

    Cleaned up champion model, can just assign to self instead of to a separate property and then assigning to self.

--- TeamFightTactics/Champions/Models/Champion.swift
+++ TeamFightTactics/Champions/Models/Champion.swift
@@ -19,29 +19,17 @@ struct Champion: DictionaryInitialize, Equatable {
     let stats: ChampionStats
     
     init(data: [String: Any]) {
-        let key = data["key"] as? String ?? ""
-        let name = data["name"] as? String ?? ""
-        let imgURL = data["imageURL"] as? String ?? ""
-        let origins = data["origins"] as? [String] ?? [""]
-        let classes = data["classes"] as? [String] ?? [""]
-        let cost = data["cost"]  as? Int ?? -1
-        let items = data["bestItems"] as? [String] ?? [""]
-        let tier = data["tier"] as? Int ?? -1
-        let patched = data["patched"] as? String
-        let ability = ChampionAbility(data: data["ability"] as? [String : Any] ?? ["": ""])
-        let stats = ChampionStats(data: data["champStats"] as? [String: Any] ?? ["": ""])
-        
-        self.key = key
-        self.name = name
-        self.imgURL = imgURL
-        self.origins = origins
-        self.classes = classes
-        self.cost = Cost(fromRawValue: cost)
-        self.bestItems = items
-        self.tier = TierRating(fromRawValue: tier)
-        self.patched = patched
-        self.ability = ability
-        self.stats = stats
+        self.key = data["key"] as? String ?? ""
+        self.name = data["name"] as? String ?? ""
+        self.imgURL = data["imageURL"] as? String ?? ""
+        self.origins = data["origins"] as? [String] ?? [""]
+        self.classes = data["classes"] as? [String] ?? [""]
+        self.cost = Cost(fromRawValue: data["cost"] as? Int ?? -1)
+        self.bestItems = data["bestItems"] as? [String] ?? [""]
+        self.tier = TierRating(fromRawValue: data["tier"] as? Int ?? -1)
+        self.patched = data["patched"] as? String
+        self.ability = ChampionAbility(data: data["ability"] as? [String : Any] ?? ["": ""])
+        self.stats = ChampionStats(data: data["champStats"] as? [String: Any] ?? ["": ""])
     }
     
     static func == (lhs: Champion, rhs: Champion) -> Bool {
@@ -49,27 +37,22 @@ struct Champion: DictionaryInitialize, Equatable {
     }
 }
 
+
 // MARK: - Champion Stats
 struct ChampionStats {
     let attackDamage, health, armor, magicResist, range: Int
     let attackSpeed: Double
     
     init(data: [String: Any]) {
-        let attDmg = data["attackDamage"] as? Int ?? -1
-        let attSpd = data["attackSpeed"] as? Double ?? Double(-1)
-        let range = data["range"] as? Int ?? -1
-        let health = data["health"] as? Int ?? -1
-        let armor = data["armor"] as? Int ?? -1
-        let magicResist = data["magicResist"] as? Int ?? -1
-        
-        self.attackDamage = attDmg
-        self.attackSpeed = attSpd
-        self.range = range
-        self.health = health
-        self.armor = armor
-        self.magicResist = magicResist
+        self.attackDamage = data["attackDamage"] as? Int ?? -1
+        self.attackSpeed = data["attackSpeed"] as? Double ?? Double(-1)
+        self.range = data["range"] as? Int ?? -1
+        self.health = data["health"] as? Int ?? -1
+        self.armor = data["armor"] as? Int ?? -1
+        self.magicResist = data["magicResist"] as? Int ?? -1
     }
 }
+
 
 // MARK: - Champion Ability
 struct ChampionAbility {
@@ -79,27 +62,21 @@ struct ChampionAbility {
     var abilityStat: [AbilityStats] = []
     
     init(data: [String: Any]) {
-        let name = data["name"] as? String ?? ""
-        let imgURL = data["imageURL"] as? String ?? ""
-        let abilityDescription = data["description"] as? String ?? ""
-        let active = data["active"] as? Bool ?? false
+        self.name = data["name"] as? String ?? ""
+        self.imgURL = data["imageURL"] as? String ?? ""
+        self.abilityDescription = data["description"] as? String ?? ""
+        self.active = data["active"] as? Bool ?? false
+        self.manaCost = data["manaCost"] as? Int ?? nil
+        self.manaStart = data["manaStart"] as? Int ?? nil
         let abilityStats = data["abilityStats"] as? [String: [Any]] ?? ["":[]]
-        let manaCost = data["manaCost"] as? Int ?? nil
-        let manaStart = data["manaStart"] as? Int ?? nil
-        
-        self.name = name
-        self.imgURL = imgURL
-        self.abilityDescription = abilityDescription
-        self.active = active
-        self.manaCost = manaCost
-        self.manaStart = manaStart
-        
+
         abilityStats.forEach { (data) in
             let abStat = AbilityStats(data: [data.key: data.value])
             self.abilityStat.append(abStat)
         }
     }
 }
+
 
 // MARK: - Ability Stat
 struct AbilityStats {
