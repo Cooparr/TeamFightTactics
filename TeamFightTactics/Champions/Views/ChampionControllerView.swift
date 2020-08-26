21ebcceeb15c3c9bb3ad722fac7d195f628cc7aa
Alexander Cooper <cooparr@hotmail.co.uk>
2020-08-26 15:53:00 +0100

    Refactored ChampionControllerView from UIView to BaseView, helps to reduce code duplication. Also now using the pinSubview function to anchor the collection view.

--- TeamFightTactics/Champions/Views/ChampionControllerView.swift
+++ TeamFightTactics/Champions/Views/ChampionControllerView.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ChampionControllerView: UIView {
+class ChampionControllerView: BaseView {
     
     //MARK:- Properties
-    var collectionView: UICollectionView = {
+    let activityIndicator = CustomActivityIndicator()
+    let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.minimumLineSpacing = 6
@@ -26,10 +27,8 @@ class ChampionControllerView: UIView {
         collectionView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 6, right: 0)
         return collectionView
     }()
-    
-    var activityIndicator = CustomActivityIndicator()
-    
-    var searchController: UISearchController = {
+
+    let searchController: UISearchController = {
         let search = UISearchController(searchResultsController: nil)
         search.obscuresBackgroundDuringPresentation = false
         search.searchBar.searchBarStyle = .minimal
@@ -39,36 +38,19 @@ class ChampionControllerView: UIView {
     }()
     
     
-    //MARK:- Init
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
-        
-        setupCollectionViewConstraints()
-        setupActivityIndicatorConstraints()
-    }
-    
-    
-    //MARK:- Constraint Functions
-    fileprivate func setupCollectionViewConstraints() {
+    //MARK: Setup View
+    override func setupView() {
         addSubview(collectionView)
-        NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: collectionView.topAnchor),
-            bottomAnchor.constraint(equalTo: collectionView.bottomAnchor),
-            leadingAnchor.constraint(equalTo: collectionView.leadingAnchor),
-            trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
-        ])
+        collectionView.pinSubview(to: self)
     }
     
-    fileprivate func setupActivityIndicatorConstraints() {
+    
+    //MARK: Setup Subviews
+    override func setupSubviews() {
         addSubview(activityIndicator)
         NSLayoutConstraint.activate([
             activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
             activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
         ])
-    }
-
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
