c80b7663675d201033ccf13ca6e7c75cf8993fb6
Alex Cooper <Cooparr@hotmail.co.uk>
2021-01-21 18:18:13 +0000

    Remove isLuxOrQiyana function, simply because does every String need access to this function? Slightly changed some parameters names.

--- TeamFightTactics/Extensions/StringExtension.swift
+++ TeamFightTactics/Extensions/StringExtension.swift
@@ -11,32 +11,27 @@ import UIKit
 
 //MARK:- String Extension
 extension String {
+    
+    //MARK: Bullet Point Property
     static var bulletPoint: String {
         return "\u{2022} "
     }
     
-    func isLuxOrQiyana() -> String {
-        if self.contains("Lux") {
-            return "Lux"
-        } else if self.contains("Qiyana") {
-            return "Qiyana"
-        } else {
-            return self
-        }
-    }
     
-    
+    //MARK: Remove Characters
     func removeCharacters(from forbiddenChars: CharacterSet) -> String {
         let passed = self.unicodeScalars.filter { !forbiddenChars.contains($0) }
         return String(String.UnicodeScalarView(passed))
     }
     
     
-    func formattedName(from: String = ".' ") -> String {
-        return removeCharacters(from: CharacterSet(charactersIn: from)).lowercased()
+    //MARK: Formatted Name
+    func formattedName(charactersToRemove: String = ".' ") -> String {
+        return removeCharacters(from: CharacterSet(charactersIn: charactersToRemove)).lowercased()
     }
     
     
+    //MARK: Developer Note
     func createDeveloperNote() -> NSAttributedString {
         let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)]
         let boldString = NSMutableAttributedString(string: "Developer Note: ", attributes: attributes)
