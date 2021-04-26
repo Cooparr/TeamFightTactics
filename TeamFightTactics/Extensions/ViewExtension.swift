9007b7a5210232bf73631881d485e5e4983e2cc4
Alex Cooper <Cooparr@hotmail.co.uk>
2021-04-26 15:12:02 +0100

    Slightly refactored the UIView extension pinSubview code. Added two new function, one allows for specific padding on the four sides, whereas the other allows for the same padding value on allSides.

--- TeamFightTactics/Extensions/ViewExtension.swift
+++ TeamFightTactics/Extensions/ViewExtension.swift
@@ -15,12 +15,33 @@ extension UIView {
         subviews.forEach { addSubview($0) }
     }
     
+    
     func pinSubview(to superView: UIView) {
         NSLayoutConstraint.activate([
             topAnchor.constraint(equalTo: superView.topAnchor),
             leadingAnchor.constraint(equalTo: superView.leadingAnchor),
             trailingAnchor.constraint(equalTo: superView.trailingAnchor),
             bottomAnchor.constraint(equalTo: superView.bottomAnchor)
+        ])
+    }
+    
+    
+    func pinSubviewWithPadding(to superView: UIView, top: CGFloat = 0, leading: CGFloat = 0, trailing: CGFloat = 0, bottom: CGFloat = 0) {
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor, constant: top),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: leading),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -trailing),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -bottom)
+        ])
+    }
+    
+    
+    func pinSubviewWithPadding(to superView: UIView, allSides: CGFloat) {
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor, constant: allSides),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: allSides),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -allSides),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -allSides)
         ])
     }
 }
