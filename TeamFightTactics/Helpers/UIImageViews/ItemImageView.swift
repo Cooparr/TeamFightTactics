5870cda70e177dc3ab3c7a3015f13bab86cfc72c
Alex Cooper <Cooparr@hotmail.co.uk>
2021-05-11 14:17:33 +0100

    Added two function one to handle adding the border and one to handle removing the border from the image view. Also changed the configureImageView function so that it accepts a String (the item name), instead of an Item object. Because some areas of the app require the use of these imageviews and the configure function but can only pass in a itemName and not the whole Item object.

--- TeamFightTactics/Helpers/UIImageViews/ItemImageView.swift
+++ TeamFightTactics/Helpers/UIImageViews/ItemImageView.swift
@@ -10,23 +10,41 @@ import UIKit
 
 class ItemImageView: UIImageView {
     
+    //MARK: Properties
+    private let borderWidth: CGFloat
+    private let borderColor: UIColor
+
     //MARK:- Init
     init(cornerRadius: CGFloat, borderWidth: CGFloat, borderColor: UIColor) {
+        self.borderWidth = borderWidth
+        self.borderColor = borderColor
         super.init(frame: .zero)
-        
         translatesAutoresizingMaskIntoConstraints = false
         clipsToBounds = true
         contentMode = .scaleAspectFit
         layer.cornerRadius = cornerRadius
         layer.borderWidth = borderWidth
-        layer.borderColor = borderColor.cgColor
     }
     
     
     //MARK:- Configure Image View
-    func configureImageView(with item: Item) {
-        image = UIImage(named: item.name.formattedName())
-        layer.borderWidth = item.isShadow == true ? 0 : 3
+    func configureImageView(with itemName: String, isShadow: Bool?) {
+        image = UIImage(named: itemName.formattedName())
+        isShadow == true ? removeBorder() : setBorder()
+    }
+    
+    
+    //MARK:  Set Border Color
+    private func setBorder() {
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColor.cgColor
+    }
+    
+    
+    //MARK:  Remove Border Color
+    private func removeBorder() {
+        layer.borderWidth = 0.0
+        layer.borderColor = nil
     }
     
     
