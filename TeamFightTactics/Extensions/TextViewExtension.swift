bb77255e64614a195d52445023d3a77ee819e6f2
Alex Cooper <Cooparr@hotmail.co.uk>
2021-06-08 15:56:39 +0100

    Created a done button extension for UITextView. This adds a toolbar above the keyboard that allows user to tap done to dismiss the keyboard.

---
+++ TeamFightTactics/Extensions/TextViewExtension.swift
@@ -0,0 +1,32 @@
+//
+//  TextViewExtension.swift
+//  TeamFightTactics
+//
+//  Created by Alexander James Cooper on 01/06/2021.
+//  Copyright © 2021 Alexander James Cooper. All rights reserved.
+//
+
+import UIKit
+
+extension UITextView {
+    
+    //MARK: Add Done Button To Keyboard
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        done.tintColor = ThemeColor.platinum
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc private func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+}
