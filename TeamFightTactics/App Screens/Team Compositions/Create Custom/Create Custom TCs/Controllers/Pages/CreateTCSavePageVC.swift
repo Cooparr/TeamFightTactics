41af261884073b03962418830f3c2c68b8de97bd
Alex Cooper <Cooparr@hotmail.co.uk>
2021-06-11 00:20:17 +0100

    Created CreateTCSavePageView to handle the view code for CreateTCSavePageVC. To conform with MVC architecture.

--- TeamFightTactics/App Screens/Team Compositions/Create Custom/Create Custom TCs/Controllers/Pages/CreateTCSavePageVC.swift
+++ TeamFightTactics/App Screens/Team Compositions/Create Custom/Create Custom TCs/Controllers/Pages/CreateTCSavePageVC.swift
@@ -16,162 +16,25 @@ protocol CreateTCSavePageDelegate: AnyObject {
 class CreateTCSavePageVC: UIViewController {
     
     //MARK: Properties
+    private(set) var savePageView = CreateTCSavePageView()
     weak var saveDelegate: CreateTCSavePageDelegate?
-    let nameView: BaseView = {
-        let view = BaseView(tamic: false, backgroundColor: ThemeColor.charcoal, cornerRadius: 10)
-        view.layer.borderWidth = 0.5
-        view.layer.borderColor = ThemeColor.romanSilver.cgColor
-        return view
-    }()
+
     
-    let nameLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.preferredFont(forTextStyle: .headline)
-        label.textColor = ThemeColor.platinum
-        label.text = "Name Team Composition"
-        return label
-    }()
-    
-    let nameTextField: UITextField = {
-        let textField = UITextField()
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.backgroundColor = ThemeColor.richBlack
-        textField.placeholder = "e.g. Six Brawlers"
-        textField.textAlignment = .center
-        textField.textColor = ThemeColor.platinum
-        textField.font = UIFont.preferredFont(forTextStyle: .body)
-        textField.adjustsFontSizeToFitWidth = true
-        textField.minimumFontSize = 12
-        textField.autocorrectionType = .no
-        textField.returnKeyType = .done
-        textField.clearButtonMode = .whileEditing
-        textField.tintColor = ThemeColor.platinum
-        textField.layer.cornerRadius = 10
-        return textField
-    }()
-    
-    let descView: BaseView = {
-        let view = BaseView(tamic: false, backgroundColor: ThemeColor.charcoal, cornerRadius: 10)
-        view.layer.borderWidth = 0.5
-        view.layer.borderColor = ThemeColor.romanSilver.cgColor
-        return view
-    }()
-    
-    let descLabel: UILabel = {
-        let lbl = UILabel()
-        lbl.translatesAutoresizingMaskIntoConstraints = false
-        lbl.font = UIFont.preferredFont(forTextStyle: .headline)
-        lbl.textColor = ThemeColor.platinum
-        lbl.text = "Describe Team Composition"
-        return lbl
-    }()    
-    
-    let descTextView: UITextView = {
-        let textView = UITextView()
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.backgroundColor = ThemeColor.richBlack
-        textView.layer.cornerRadius = 10
-        textView.font = UIFont.preferredFont(forTextStyle: .body)
-        textView.tintColor = ThemeColor.platinum
-        textView.addDoneButtonOnKeyboard()
-        return textView
-    }()
-    
-    let saveButton: UIButton = {
-        let btn = UIButton()
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.translatesAutoresizingMaskIntoConstraints = false
-        btn.backgroundColor = ThemeColor.romanSilver
-        btn.setTitle("Save Team Comp", for: .normal)
-        btn.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
-        btn.setTitleColor(ThemeColor.platinum, for: .normal)
-        btn.layer.cornerRadius = 10
-        return btn
-    }()
+    //MARK: Load View
+    override func loadView() {
+        super.loadView()
+        self.view = savePageView
+    }
     
     
     //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = ThemeColor.richBlack
         createDismissKeyboardTapGesture()
-        
-        
         addKeyboardObservers()
-        
-        nameTextField.delegate = self
-        
-        constrainNameView()
-        constrainSaveButton()
-        constrainDescView()
-        
-        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
-    }
-    
-    
-    //MARK:- Constraint Code
-    private func constrainNameView() {
-        view.addSubview(nameView)
-        NSLayoutConstraint.activate([
-            nameView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            nameView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            nameView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
-        ])
-        
-        nameView.addSubviews(nameLabel, nameTextField)
-        NSLayoutConstraint.activate([
-            nameLabel.topAnchor.constraint(equalTo: nameView.topAnchor, constant: 5),
-            nameLabel.leadingAnchor.constraint(equalTo: nameView.leadingAnchor, constant: 10),
-            nameLabel.trailingAnchor.constraint(equalTo: nameView.trailingAnchor, constant: -10),
-            nameLabel.bottomAnchor.constraint(equalTo: nameTextField.topAnchor, constant: -5),
-            nameLabel.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
-        NSLayoutConstraint.activate([
-            nameTextField.heightAnchor.constraint(equalToConstant: 40),
-            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
-            nameTextField.leadingAnchor.constraint(equalTo: nameView.leadingAnchor, constant: 10),
-            nameTextField.trailingAnchor.constraint(equalTo: nameView.trailingAnchor, constant: -10),
-            nameTextField.bottomAnchor.constraint(equalTo: nameView.bottomAnchor, constant: -10)
-        ])
-    }
-    
-    
-    private func constrainSaveButton() {
-        view.addSubview(saveButton)
-        NSLayoutConstraint.activate([
-            saveButton.heightAnchor.constraint(equalToConstant: 50),
-            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            saveButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40)
-        ])
-    }
-    
-    private func constrainDescView() {
-        view.addSubview(descView)
-        NSLayoutConstraint.activate([
-            descView.topAnchor.constraint(equalTo: nameView.bottomAnchor, constant: 20),
-            descView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            descView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            descView.bottomAnchor.constraint(equalTo: saveButton.topAnchor, constant: -20)
-        ])
-        
-        descView.addSubviews(descLabel, descTextView)
-        NSLayoutConstraint.activate([
-            descLabel.topAnchor.constraint(equalTo: descView.topAnchor, constant: 5),
-            descLabel.leadingAnchor.constraint(equalTo: descView.leadingAnchor, constant: 10),
-            descLabel.trailingAnchor.constraint(equalTo: descView.trailingAnchor, constant: -10),
-            descLabel.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
-        NSLayoutConstraint.activate([
-            descTextView.topAnchor.constraint(equalTo: descLabel.bottomAnchor, constant: 5),
-            descTextView.leadingAnchor.constraint(equalTo: descView.leadingAnchor, constant: 10),
-            descTextView.trailingAnchor.constraint(equalTo: descView.trailingAnchor, constant: -10),
-            descTextView.bottomAnchor.constraint(equalTo: descView.bottomAnchor, constant: -10)
-        ])
+        savePageView.nameTextField.delegate = self
+        savePageView.saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
     }
     
     
@@ -191,16 +54,16 @@ class CreateTCSavePageVC: UIViewController {
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
 
         if notification.name == UIResponder.keyboardWillHideNotification {
-            descTextView.contentInset = .zero
+            savePageView.descTextView.contentInset = .zero
         } else {
             let saveButtonHeightAndPadding: CGFloat = 110
-            descTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: (keyboardViewEndFrame.height - view.safeAreaInsets.bottom) - saveButtonHeightAndPadding, right: 0)
+            savePageView.descTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: (keyboardViewEndFrame.height - view.safeAreaInsets.bottom) - saveButtonHeightAndPadding, right: 0)
         }
 
-        descTextView.scrollIndicatorInsets = descTextView.contentInset
+        savePageView.descTextView.scrollIndicatorInsets = savePageView.descTextView.contentInset
 
-        let selectedRange = descTextView.selectedRange
-        descTextView.scrollRangeToVisible(selectedRange)
+        let selectedRange = savePageView.descTextView.selectedRange
+        savePageView.descTextView.scrollRangeToVisible(selectedRange)
     }
     
     
@@ -215,8 +78,8 @@ class CreateTCSavePageVC: UIViewController {
     //MARK:- Save Button Tap Action
     @objc func saveButtonTapped(button: UIButton) {
         button.pulseAnimateOnTap()
-        guard let teamCompTitle = nameTextField.text else { return }
-        let teamCompDesc = descTextView.text
+        guard let teamCompTitle = savePageView.nameTextField.text else { return }
+        let teamCompDesc = savePageView.descTextView.text
         guard let teamCompChamps = saveDelegate?.getChampionsForTeamComp() else { return }
         guard let teamCompTraits = saveDelegate?.getTraitsForTeamComp() else { return }
         let teamCompToSave = CustomTeamComposition(title: teamCompTitle, description: teamCompDesc, champions: teamCompChamps, traits: teamCompTraits)
@@ -233,7 +96,7 @@ class CreateTCSavePageVC: UIViewController {
     }
 }
 
-//MARK: UITextfield Delegate Methods
+//MARK: UITextField Delegate Methods
 extension CreateTCSavePageVC: UITextFieldDelegate {
         
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
---
+++ TeamFightTactics/App Screens/Team Compositions/Create Custom/Create Custom TCs/Views/Child VC Views/CreateTCSavePageView.swift
@@ -0,0 +1,134 @@
+//
+//  CreateTCSavePageView.swift
+//  TeamFightTactics
+//
+//  Created by Alexander James Cooper on 09/06/2021.
+//  Copyright © 2021 Alexander James Cooper. All rights reserved.
+//
+
+import UIKit
+
+class CreateTCSavePageView: UIView {
+    
+    //MARK:- Properties
+    let contentsStack = BaseStack(axis: .vertical, distribution: .fill, alignment: .leading, spacing: 20)
+    let nameSectionContainerView: BaseView = {
+        let view = BaseView(tamic: false, backgroundColor: ThemeColor.charcoal, cornerRadius: 10)
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = ThemeColor.romanSilver.cgColor
+        return view
+    }()
+    
+    let nameLabel = BaseLabel(textStyleFont: .headline, text: "Name Team Composition")
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = ThemeColor.richBlack
+        textField.placeholder = "e.g. Six Brawlers"
+        textField.textAlignment = .center
+        textField.textColor = ThemeColor.platinum
+        textField.font = UIFont.preferredFont(forTextStyle: .body)
+        textField.adjustsFontSizeToFitWidth = true
+        textField.minimumFontSize = 12
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .done
+        textField.clearButtonMode = .whileEditing
+        textField.tintColor = ThemeColor.platinum
+        textField.layer.cornerRadius = 10
+        return textField
+    }()
+    
+    let descSectionContainerView: BaseView = {
+        let view = BaseView(tamic: false, backgroundColor: ThemeColor.charcoal, cornerRadius: 10)
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = ThemeColor.romanSilver.cgColor
+        return view
+    }()
+    
+    let descLabel = BaseLabel(textStyleFont: .headline, text: "Describe Team Composition")
+    let descTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = ThemeColor.richBlack
+        textView.layer.cornerRadius = 10
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.tintColor = ThemeColor.platinum
+        textView.addDoneButtonOnKeyboard()
+        return textView
+    }()
+    
+    let saveButton: BaseButton = {
+        let btn = BaseButton(textStyle: .headline, cornerRadius: 10)
+        btn.setTitle("Save Team Comp", for: .normal)
+        return btn
+    }()
+    
+    
+    //MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = ThemeColor.richBlack
+        constrainContentStackView()
+        constrainNameView()
+        constrainDescView()
+    }
+    
+    
+    //MARK:- Constraint Code
+    private func constrainContentStackView() {
+        addSubview(contentsStack)
+        contentsStack.addArrangedSubviews(nameSectionContainerView, descSectionContainerView, saveButton)
+        contentsStack.pinSubviewWithPadding(to: self, top: 10, leading: 10, trailing: -10, bottom: -40)
+        
+        NSLayoutConstraint.activate([
+            nameSectionContainerView.widthAnchor.constraint(equalTo: contentsStack.widthAnchor),
+            descSectionContainerView.widthAnchor.constraint(equalTo: contentsStack.widthAnchor),
+            saveButton.heightAnchor.constraint(equalToConstant: 50),
+            saveButton.widthAnchor.constraint(equalTo: contentsStack.widthAnchor)
+        ])
+    }
+    
+    
+    private func constrainNameView() {
+        nameSectionContainerView.addSubviews(nameLabel, nameTextField)
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: nameSectionContainerView.topAnchor, constant: 5),
+            nameLabel.leadingAnchor.constraint(equalTo: nameSectionContainerView.leadingAnchor, constant: 10),
+            nameLabel.trailingAnchor.constraint(equalTo: nameSectionContainerView.trailingAnchor, constant: -10),
+            nameLabel.bottomAnchor.constraint(equalTo: nameTextField.topAnchor, constant: -5),
+            nameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+
+        NSLayoutConstraint.activate([
+            nameTextField.heightAnchor.constraint(equalToConstant: 40),
+            nameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5),
+            nameTextField.leadingAnchor.constraint(equalTo: nameSectionContainerView.leadingAnchor, constant: 10),
+            nameTextField.trailingAnchor.constraint(equalTo: nameSectionContainerView.trailingAnchor, constant: -10),
+            nameTextField.bottomAnchor.constraint(equalTo: nameSectionContainerView.bottomAnchor, constant: -10)
+        ])
+    }
+    
+    
+    private func constrainDescView() {
+        descSectionContainerView.addSubviews(descLabel, descTextView)
+        NSLayoutConstraint.activate([
+            descLabel.topAnchor.constraint(equalTo: descSectionContainerView.topAnchor, constant: 5),
+            descLabel.leadingAnchor.constraint(equalTo: descSectionContainerView.leadingAnchor, constant: 10),
+            descLabel.trailingAnchor.constraint(equalTo: descSectionContainerView.trailingAnchor, constant: -10),
+            descLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+
+        NSLayoutConstraint.activate([
+            descTextView.topAnchor.constraint(equalTo: descLabel.bottomAnchor, constant: 5),
+            descTextView.leadingAnchor.constraint(equalTo: descSectionContainerView.leadingAnchor, constant: 10),
+            descTextView.trailingAnchor.constraint(equalTo: descSectionContainerView.trailingAnchor, constant: -10),
+            descTextView.bottomAnchor.constraint(equalTo: descSectionContainerView.bottomAnchor, constant: -10)
+        ])
+    }
+    
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
