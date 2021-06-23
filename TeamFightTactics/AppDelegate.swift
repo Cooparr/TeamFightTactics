302307fe81ca2c78fb6fe0ee1be1560d90ad2de5
Alex Cooper <Cooparr@hotmail.co.uk>
2021-06-23 10:46:58 +0100

    Added signInAnonymously

--- TeamFightTactics/AppDelegate.swift
+++ TeamFightTactics/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import FirebaseCore
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Auth.auth().signInAnonymously()
+        
         isFirstTimeLaunchingApp()
         setGlobalCustomisations()
         
