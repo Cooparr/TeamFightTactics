cdc44361c02235e335804c8c6fca688f840c2764
Alex Cooper <Cooparr@hotmail.co.uk>
2021-07-01 11:44:41 +0100

    Removed Deinit

--- TeamFightTactics/Firebase/SetDataManager.swift
+++ TeamFightTactics/Firebase/SetDataManager.swift
@@ -86,12 +86,6 @@ class SetDataManager {
             onCompletion(.failure(SetDataError.localFetchError))
         }
     }
-    
-    
-
-    deinit {
-        print("Manager was deinit'd")
-    }
 }
 
 
