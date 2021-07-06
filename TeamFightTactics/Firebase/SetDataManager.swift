94c2edd8066b5a395752726f2f18beacc35a307d
Alex Cooper <Cooparr@hotmail.co.uk>
2021-07-06 13:19:12 +0100

    Added Type Aliases to heated function headers. Also created a setCurrentPatchVersion() which fetched the latest patch version number from Firebase and sets this to UserDefaults.

--- TeamFightTactics/Firebase/SetDataManager.swift
+++ TeamFightTactics/Firebase/SetDataManager.swift
@@ -10,9 +10,13 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 
 class SetDataManager {
-
+    
+    //MARK: Type Aliases
+    typealias FetchDataHandler<FetchedItem> = (Result<[FetchedItem], SetDataError>) -> Void
+    typealias FetchResult<FetchedItem> = Result<[FetchedItem], SetDataError>
+    
+    
     //MARK: Properties
-    private let currentInGameSet = TFTSet.five
     private var selectedSet: TFTSet {
         guard let selectedSet = TFTSet(rawValue: UserDefaults.standard.double(forKey: UDKey.setKey)) else {
             print("Error creating TFTSet value from UserDefaults - Setting to default value")
@@ -30,11 +34,10 @@ class SetDataManager {
     
     
     //MARK: Fetch Set Data
-    func fetchData<FetchedItem: Decodable>(from collection: Collection, _ onCompletion: @escaping (Result<[FetchedItem], Error>) -> Void) -> ListenerRegistration? {
-        
+    func fetchData<FetchedItem: Decodable>(from collection: Collection, _ onCompletion: @escaping FetchDataHandler<FetchedItem>) -> ListenerRegistration? {
         switch selectedSet {
         case .one, .two, .three, .four, .four_5:
-            fetchFromLocal(withFileName: collection) { (result: Result<[FetchedItem], Error>) in
+            fetchFromLocal(withFileName: collection) { (result: FetchResult<FetchedItem>) in
                 switch result {
                 case .success (let localData):
                     onCompletion(.success(localData))
@@ -46,7 +49,7 @@ class SetDataManager {
             return nil
             
         case .five:
-            let listener = fetchFromFirebase(in: collection) { (result: Result<[FetchedItem], Error>) in
+            let listener = fetchFromFirebase(in: collection) { (result: FetchResult<FetchedItem>) in
                 switch result {
                 case .success (let fetchedData):
                     onCompletion(.success(fetchedData))
@@ -60,10 +63,13 @@ class SetDataManager {
     
 
     //MARK: Fetch From Firebase
-    private func fetchFromFirebase<FirestoreItem: Decodable>(in collection: Collection, _ onCompletion: @escaping (Result<[FirestoreItem], Error>) -> Void) -> ListenerRegistration {
+    private func fetchFromFirebase<FirestoreItem: Decodable>(in collection: Collection, _ onCompletion: @escaping FetchDataHandler<FirestoreItem>) -> ListenerRegistration {
         let listener = getSetDataCollectionReference(collection).addSnapshotListener { querySnapshot, error in
-            if let error = error { return onCompletion(.failure(error)) }
-            guard let documents = querySnapshot?.documents else { return onCompletion(.failure(SetDataError.unwrapDocumentsError)) }
+            if let error = error {
+                print(error)
+                return onCompletion(.failure(.firebaseError))
+            }
+            guard let documents = querySnapshot?.documents else { return onCompletion(.failure(.unwrapDocumentsError)) }
             let fetchedData = documents.compactMap { try? $0.data(as: FirestoreItem.self) }
             onCompletion(.success(fetchedData))
         }
@@ -72,7 +78,7 @@ class SetDataManager {
 
 
     //MARK: Fetch From Local
-    private func fetchFromLocal<LocalItem: Decodable>(withFileName: Collection, _ onCompletion: @escaping (Result<[LocalItem], Error>) -> Void) {
+    private func fetchFromLocal<LocalItem: Decodable>(withFileName: Collection, _ onCompletion: @escaping FetchDataHandler<LocalItem>) {
         guard let path = Bundle.main.path(forResource: withFileName.rawValue, ofType: "json", inDirectory: "/LocalData/\(selectedSet.rawValue)") else { return }
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
@@ -83,7 +89,30 @@ class SetDataManager {
             let localItems = try jsonDecoder.decode([LocalItem].self, from: data)
             onCompletion(.success(localItems))
         } catch {
-            onCompletion(.failure(SetDataError.localFetchError))
+            onCompletion(.failure(.localFetchError))
+        }
+    }
+    
+    
+    //MARK: Set Current Patch Version
+    func setCurrentPatchVersion() {
+        guard let latestSet = TFTSet.allCases.last?.rawValue else { return }
+        Firestore.firestore().collection(Collection.setData.rawValue).document(String(latestSet)).collection(Collection.patchNotes.rawValue)
+            .order(by: FieldValues.date.rawValue, descending: true).limit(to: 1)
+            .getDocuments { querySnapshot, error in
+                if let error = error {
+                    print(error)
+                    return print(SetDataError.firebaseError.rawValue)
+                }
+                
+                guard let patchDoc = querySnapshot?.documents.first else {
+                    return print(SetDataError.firstPatchDocError.rawValue)
+                }
+                guard let currentPatchVersion = patchDoc.get(FieldValues.version.rawValue) as? String else {
+                    return print(SetDataError.getPatchVersionError.rawValue)
+                }
+                
+                UserDefaults.standard.setValue(currentPatchVersion, forKey: UDKey.currentPatch)
         }
     }
 }
@@ -93,24 +122,32 @@ class SetDataManager {
 extension SetDataManager {
     
     enum Collection: String {
-        case setData = "SetData"
-        case items = "Items"
-        case champions = "Champions"
-        case teamComps = "TeamComps"
-        case classes = "Classes"
-        case origins = "Origins"
-        case dropRates = "DropRates"
+        case setData    = "SetData"
+        case items      = "Items"
+        case champions  = "Champions"
+        case teamComps  = "TeamComps"
+        case classes    = "Classes"
+        case origins    = "Origins"
+        case dropRates  = "DropRates"
         case patchNotes = "PatchNotes"
-        case galaxies = "Galaxies"
+        case galaxies   = "Galaxies"
+    }
+    
+    enum FieldValues: String {
+        case date       = "date"
+        case version    = "version"
     }
 }
 
 
 enum SetDataError: String, Error, LocalizedError {
-    case unexpectedError = "An unexecpted error has occured."
-    case localFetchError = "An error has occured when trying to locally fetch the requested set data."
-    case firebaseFetchError = "An error has occured when trying to fetch the requested set data from the server."
-    case unwrapDocumentsError = "Error occured when unwrapping snapshot documents."
+    case unexpectedError        = "An unexecpted error has occured."
+    case localFetchError        = "An error has occured when trying to locally fetch the requested set data."
+    case firebaseError          = "An error was thrown by Cloud Firestore."
+    case firebaseFetchError     = "An error has occured when trying to fetch the requested set data from the server."
+    case unwrapDocumentsError   = "Error occured when unwrapping snapshot documents."
+    case firstPatchDocError     = "Error unwrapping first patch document"
+    case getPatchVersionError   = "Error getting 'version' field value from patch document"
     
     public var errorDescription: String? {
         return self.rawValue
