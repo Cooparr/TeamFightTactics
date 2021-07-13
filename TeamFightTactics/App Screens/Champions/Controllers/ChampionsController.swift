b1ea7dd9b1a83a2a0b3b4e23941d88404a129bbc
Alex Cooper <Cooparr@hotmail.co.uk>
2021-07-13 15:50:12 +0100

    Deleted old delegate flow layout code conformance.

--- TeamFightTactics/App Screens/Champions/Controllers/ChampionsController.swift
+++ TeamFightTactics/App Screens/Champions/Controllers/ChampionsController.swift
@@ -38,7 +38,6 @@ class ChampionsController: UIViewController {
         navigationBarSetup()
         
         // Assign Delegates
-        champRootView.collectionView.delegate = self
         champRootView.collectionView.dataSource = self
         champRootView.searchController.searchBar.delegate = self
     }
@@ -85,11 +84,11 @@ class ChampionsController: UIViewController {
 
 //MARK:- CollectionView Data Source
 extension ChampionsController: UICollectionViewDataSource {
-    
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return filteredChampions.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(ChampionCell.self, for: indexPath)
         cell.configureCell(with: filteredChampions[indexPath.item])
@@ -97,50 +96,6 @@ extension ChampionsController: UICollectionViewDataSource {
     }
 }
 
-
-//MARK:- CollectionView Delegate Flow Layout
-extension ChampionsController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        guard let cell = collectionView.cellForItem(at: indexPath) as? ChampionCell else { return }
-        let statBaseValues = [allChampions[indexPath.item].stats.health, allChampions[indexPath.item].stats.attackDamage]
-        let statLabels = [cell.baseStats.healthStat.statLabel, cell.baseStats.attDmgStat.statLabel]
-        for (i, statLabel) in statLabels.enumerated() {
-            guard let labelValue = statLabels[i].text else { return }
-            let level2 = Int(Double(statBaseValues[i]) * 1.8)
-            let level3 = Int(Double(statBaseValues[i]) * 3.6)
-            UIView.transition(with: statLabel, duration: 0.8, options: .transitionFlipFromBottom, animations: {
-                switch Int(labelValue) {
-                case level2:
-                    statLabels[i].text = "\(level3)"
-                case level3:
-                    statLabels[i].text = "\(statBaseValues[i])"
-                default:
-                    statLabels[i].text = "\(level2)"
-                }
-            })
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let champAbilityText: String = self.filteredChampions[indexPath.item].ability.description
-        let heightPad: CGFloat = 121
-        let widthPad: CGFloat = 60
-        let approxAbilityDescWidth: CGFloat = view.frame.width - widthPad
-        let size: CGSize = CGSize(width: approxAbilityDescWidth, height: 1000)
-        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 11)]
-        let estimatedFrame = NSString(string: champAbilityText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
-        
-        let setWidth: CGFloat = view.frame.width - 8
-        var setHeight: CGFloat = estimatedFrame.height + heightPad
-        
-        if estimatedFrame.height + heightPad < 120 {
-            setHeight = 120
-        }
-        
-        return CGSize(width: setWidth, height: setHeight)
-    }   
-}
 
 //MARK:- Search Bar Delegate
 extension ChampionsController: UISearchBarDelegate {
